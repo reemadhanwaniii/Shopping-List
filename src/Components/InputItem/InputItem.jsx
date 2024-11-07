a7dd7cec1e0f844550b4833c7f17e3a0748bef7e
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-11-07 21:16:49 +0530

    use react form hook library

--- src/Components/InputItem/InputItem.jsx
+++ src/Components/InputItem/InputItem.jsx
@@ -1,33 +1,43 @@
 import './InputItem.css';
 
 import { showSuccess } from '../Utils/showToasts';
-import { useState } from 'react';
+import { useForm } from "react-hook-form";
 
 
 function InputItem({addItem}){
 
-    const [itemName,setItemName] = useState('')
+   
+    const { register, handleSubmit, formState: { errors }} = useForm(); 
+
+    const handleSubmission = (data) => {
+        // e.preventDefault();
+        // addItem(itemName);
+        // setItemName("");
+        addItem(data.item)
+        showSuccess("Successfully added Item");
+    }
 
     return(
         <>
             <div className="item-input-wrapper">
-                <input
-                    className='item-input'
-                    type="text"
-                    placeholder="Add an Item"
-                    value={itemName}
-                    onChange={(e)=>setItemName(e.target.value)}
-                />
-                <button 
-                    className='add-item-btn'
-                    onClick={() => {
-                        showSuccess("Successfully added Item");
-                        addItem(itemName);
-                        setItemName("");
-                    }}
-                >
-                    Add
-                </button>
+                <form onSubmit={handleSubmit(handleSubmission)}>
+                    <input
+                        className='item-input'
+                        type="text"
+                        placeholder="Add an Item"
+                        {...register("item",{ required: true, minLength: 3 })}
+                    />
+                    
+                    <button 
+                        className='add-item-btn'
+                    >
+                        Add
+                    </button>
+               </form>
+               <div>
+                        {errors.item && errors.item.type === "required" && <p>Item is missing</p>}
+                        {errors.item && errors.item.type === "minLength" && <p>Item length cannot be less than 3</p>}
+                </div>
             </div>
         </>
     );
